a67d3db422e59a3d9c4b701ee29e48bd416b688a
Grigory Fetkulin <103204349+Fetkulingr@users.noreply.github.com>
2023-03-19 14:06:33 +0300

    Добавлена клавиатура

--- main.py
+++ main.py
@@ -3,44 +3,77 @@ import telebot
 from settings import TG_TOKEN
 # Подключение API ключа
 bot = telebot.TeleBot(TG_TOKEN)
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Что будем делать? Путешествовать или изучать историю?")
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-# Покупка билетов
-    if message.text =="Путешествовать":
-        bot.reply_to(message, "Самолёт или Поезд?")
-    elif message.text == "Самолёт":
-        bot.reply_to(message,"Неземное путешествие по земным ценам: https://www.aeroflot.ru/ru-ru")
-    elif message.text == "Поезд":
-        bot.reply_to(message, "Лучшие железнодорожные путешествия только здесь: https://www.rzd.ru/")
-# Бронирование отеля
-    elif message.text == "Хочу забронировать отель":
-        bot.reply_to(message, "Лучшие отели только здесь: https://ostrovok.ru/")
-    elif message.text == "Изучать историю":
-        bot.reply_to(message, "Историю какого города желаете изучить?")
-# Список городов
-    elif message.text == "Москва":
-        bot.reply_to(message, "https://ru.wikipedia.org/wiki/%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0")
-    elif message.text == "Санкт-Петербург":
-        bot.reply_to(message, "https://ru.wikipedia.org/wiki/%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3")
-    elif message.text == "Нижний Новгород":
-        bot.reply_to(message, "https://ru.wikipedia.org/wiki/%D0%9D%D0%B8%D0%B6%D0%BD%D0%B8%D0%B9_%D0%9D%D0%BE%D0%B2%D0%B3%D0%BE%D1%80%D0%BE%D0%B4")
-    elif message.text == "Сергиев Посад":
-        bot.reply_to(message, "https://ru.wikipedia.org/wiki/%D0%A1%D0%B5%D1%80%D0%B3%D0%B8%D0%B5%D0%B2_%D0%9F%D0%BE%D1%81%D0%B0%D0%B4")
-    elif message.text == "Переславль":
-        bot.reply_to(message, "https://ru.wikipedia.org/wiki/%D0%9F%D0%B5%D1%80%D0%B5%D1%81%D0%BB%D0%B0%D0%B2%D0%BB%D1%8C-%D0%97%D0%B0%D0%BB%D0%B5%D1%81%D1%81%D0%BA%D0%B8%D0%B9")
-    elif message.text == "Ростов":
-        bot.reply_to(message, "https://ru.wikipedia.org/wiki/%D0%A0%D0%BE%D1%81%D1%82%D0%BE%D0%B2")
-    elif message.text == "Кострома":
-        bot.reply_to(message, "https://ru.wikipedia.org/wiki/%D0%9A%D0%BE%D1%81%D1%82%D1%80%D0%BE%D0%BC%D0%B0")
-    elif message.text == "Иваново":
-        bot.reply_to(message, "https://ru.wikipedia.org/wiki/%D0%98%D0%B2%D0%B0%D0%BD%D0%BE%D0%B2%D0%BE")
-    elif message.text == "Суздаль":
-        bot.reply_to(message, "https://ru.wikipedia.org/wiki/%D0%A1%D1%83%D0%B7%D0%B4%D0%B0%D0%BB%D1%8C")
-    elif message.text == "Владимир":
-        bot.reply_to(message, "https://ru.wikipedia.org/wiki/%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%BC%D0%B8%D1%80_(%D0%B3%D0%BE%D1%80%D0%BE%D0%B4,_%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F")
-        bot.infinity_polling()
+ # Функция, обрабатывающая команду /start
+@bot.message_handler(commands=['start'])
+def welcome(message):
+ 
+    # клавиатура
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Путешествовать")
+    btn2 = types.KeyboardButton("Изучать историю")
+ 
+    markup.add(btn1, btn2)
+ 
+    bot.send_message(message.chat.id, "Здравствуйте, {0.first_name}!\nМеня зовут<b> {1.first_name}</b>, Что будем делать? Путешествовать или изучать историю?".format(message.from_user, bot.get_me()),
+        parse_mode='html', reply_markup=markup)
+ 
+@bot.message_handler(content_types=['text'])
+def lalala(message):
+    if message.chat.type == 'private':
+        if message.text == 'Путешествовать':
+            # Купить авиа/жд билеты, бронирование отеля:
+            bot.send_message(message.chat.id, "https://www.tutu.ru/")
+        elif message.text == 'Изучать историю':
+ 
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            btn1 = types.InlineKeyboardButton("Москва", callback_data='city_1')
+            btn2 = types.InlineKeyboardButton("Санкт-Петербург", callback_data='city_2')
+            btn3 = types.InlineKeyboardButton("Нижний Новгород", callback_data='city_3')
+            btn4 = types.InlineKeyboardButton("Сергиев Посад", callback_data='city_4')
+            btn5 = types.InlineKeyboardButton("Переславль", callback_data='city_5')
+            btn6 = types.InlineKeyboardButton("Ростов", callback_data='city_6')
+            btn7 = types.InlineKeyboardButton("Кострома", callback_data='city_7')
+            btn8 = types.InlineKeyboardButton("Иваново", callback_data='city_8')
+            btn9 = types.InlineKeyboardButton("Суздаль", callback_data='city_9')
+            btn10 = types.InlineKeyboardButton("Владимир", callback_data='city_10')
 
-bot.infinity_polling()
+ 
+            markup.add(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10)
+ 
+            bot.send_message(message.chat.id, 'Историю какого города желаете изучить?', reply_markup=markup)
+ 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    # Список городов
+    try:
+        if call.message:
+            if call.data == 'city_1':
+                bot.send_message(call.message.chat.id, 'https://ru.wikipedia.org/wiki/%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0')
+            elif call.data == 'city_2':
+                bot.send_message(call.message.chat.id, 'https://ru.wikipedia.org/wiki/%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3')
+            elif call.data == 'city_3':
+                bot.send_message(call.message.chat.id, 'https://ru.wikipedia.org/wiki/%D0%9D%D0%B8%D0%B6%D0%BD%D0%B8%D0%B9_%D0%9D%D0%BE%D0%B2%D0%B3%D0%BE%D1%80%D0%BE%D0%B4')
+            elif call.data == 'city_4':
+                bot.send_message(call.message.chat.id, 'https://ru.wikipedia.org/wiki/%D0%A1%D0%B5%D1%80%D0%B3%D0%B8%D0%B5%D0%B2_%D0%9F%D0%BE%D1%81%D0%B0%D0%B4')
+            elif call.data == 'city_5':
+                bot.send_message(call.message.chat.id, 'https://ru.wikipedia.org/wiki/%D0%9F%D0%B5%D1%80%D0%B5%D1%81%D0%BB%D0%B0%D0%B2%D0%BB%D1%8C-%D0%97%D0%B0%D0%BB%D0%B5%D1%81%D1%81%D0%BA%D0%B8%D0%B9')
+            elif call.data == 'city_6':
+                bot.send_message(call.message.chat.id, 'https://ru.wikipedia.org/wiki/%D0%A0%D0%BE%D1%81%D1%82%D0%BE%D0%B2')
+            elif call.data == 'city_7':
+                bot.send_message(call.message.chat.id, 'https://ru.wikipedia.org/wiki/%D0%9A%D0%BE%D1%81%D1%82%D1%80%D0%BE%D0%BC%D0%B0')
+            elif call.data == 'city_8':
+                bot.send_message(call.message.chat.id, 'https://ru.wikipedia.org/wiki/%D0%98%D0%B2%D0%B0%D0%BD%D0%BE%D0%B2%D0%BE')
+            elif call.data == 'city_9':
+                bot.send_message(call.message.chat.id, 'https://ru.wikipedia.org/wiki/%D0%A1%D1%83%D0%B7%D0%B4%D0%B0%D0%BB%D1%8C')
+            elif call.data == 'city_10':
+                bot.send_message(call.message.chat.id, 'https://ru.wikipedia.org/wiki/%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%BC%D0%B8%D1%80_(%D0%B3%D0%BE%D1%80%D0%BE%D0%B4,_%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F)')
+    # удалить встроенные кнопки
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Приятного изучения!",
+                reply_markup=None)
+ 
+
+    except Exception as e:
+        print(repr(e))
+ 
+# Запуск бота
+bot.polling(none_stop=True)
